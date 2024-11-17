b30c6fff9aefe6c4d35392bf7519c417c5ebc624
matteoltz <matteo.lutz06@outlook.de>
2024-11-17 09:22:22 +0100

    updated run script

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "[EVO-NFC] Hello!"
+
+echo "[EVO-NFC] Checking if venv exists.."
+if [ ! -d "venv" ]; then
+    echo "[EVO-NFC] Creating venv.."
+    python3 -m venv venv
+fi
+
+echo "[EVO-NFC] Activating venv.."
 source venv/bin/activate
 
-echo "[EVO-NFC] Starting NFC.."
 echo "[EVO-NFC] Installing dependencies.."
 pip3 install -r requirements.txt
 
